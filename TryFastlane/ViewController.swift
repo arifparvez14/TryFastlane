c6242e030abc0da0f4297e69a8553de104d511d0
Arif <arif.mbstu22@gmail.com>
2019-12-20 15:11:18 +0600

    check
    check

--- TryFastlane/ViewController.swift
+++ TryFastlane/ViewController.swift
@@ -83,7 +83,7 @@ class ViewController: UIViewController {
     }
     
     func FunctinTest2(value1: Int, Value2: Int) -> Int {
-        print("Hello World")
+        print("Hello Worldwww")
         return value1 - Value2
     }
   
